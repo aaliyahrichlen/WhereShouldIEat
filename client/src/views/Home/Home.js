25d7e2a662277cd58e21ba43dea6e25b85d2ffe3
aaliyahrichlen <aaliyahrichlen@ufl.edu>
2020-05-22 22:53:00 -0400

    testing heruko updating

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -8,6 +8,9 @@ function Home() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
+                    This is the home page!
+                </p>
+                <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
